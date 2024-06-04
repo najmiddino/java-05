d9ea6e966c342d9c9f514036ff6d2bbee07522ce
najmiddino <164999098+najmiddino@users.noreply.github.com>
2024-06-04 14:55:45 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let str = prompt('ismingizni kiriting')
+let b = prompt('harifkiriting')
+let f =(b.toLowerCase)
+let v = (str.toLowerCase)
+ if (str.includes(b)) {
+alert (`${str}isimdan ${b} harifi mavjud`)
+ } else {
+    alert (`${str}isimdan ${b} harifi mavjud emas`)
+ }
